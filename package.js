1a81b237d8147815a5fb2cdde7eff5885b395054
Leon Machens <leon.machens@googlemail.com>
2018-10-03 17:41:08 +0200

    fix runtime version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lmachens:ecmascript-ts',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Compiler plugin that supports ES2015+ in all .ts and .tsx files',
   documentation: 'README.md',
   git: 'https://github.com/lmachens/ecmascript-ts',
