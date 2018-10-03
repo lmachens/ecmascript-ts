a33b900b16d6113d3bd0fa313708a3f937970ed8
Leon Machens <leon.machens@googlemail.com>
2018-10-03 17:24:50 +0200

    fix version

--- package.js
+++ package.js
@@ -2,7 +2,8 @@ Package.describe({
   name: 'lmachens:ecmascript-ts',
   version: '1.0.0',
   summary: 'Compiler plugin that supports ES2015+ in all .ts and .tsx files',
-  documentation: 'README.md'
+  documentation: 'README.md',
+  git: 'https://github.com/lmachens/ecmascript-ts',
 });
 
 Package.registerBuildPlugin({
@@ -13,13 +14,13 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('babel-compiler');
+  api.use('babel-compiler@7.1.1');
 
   // `ecmascript-ts` must provide the same runtime environment
   // that the 'ecmascript' package provides.
-  api.imply('modules');
-  api.imply('ecmascript-runtime');
-  api.imply('babel-runtime');
-  api.imply('promise');
-  api.imply('dynamic-import');
+  api.imply('modules@0.12.2');
+  api.imply('ecmascript-runtime@0.5.0');
+  api.imply('babel-runtime@1.2.2');
+  api.imply('promise@0.10.1');
+  api.imply('dynamic-import@0.3.0');
 });
